26fada0f38b8bedd8174947d9dd741ecaa3d5a7f
tlpackus <tlpackus@gmail.com>
2021-03-28 01:30:15 -0700

    rename file and use async function to call api

---
+++ src/currency-converter.js
@@ -0,0 +1,15 @@
+export default class Currency {
+  static async getConversions() {
+    try {
+      const response = await fetch (`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD
+      `);
+
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    } catch (error) {
+      return error.message;
+    }
+  }
+}
