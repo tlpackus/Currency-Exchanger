802e9f7828f158c1c9ba2e869ddac16d6ee5fbcf
tlpackus <tlpackus@gmail.com>
2021-04-09 16:04:18 -0700

    update titles to currency-exchanger

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Example Project',
+      title: 'Currency-Exchanger',
       template: './src/index.html',
       inject: 'body'
     })
