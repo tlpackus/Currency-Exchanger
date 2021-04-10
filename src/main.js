21ca7e58edc1bf82ade6a14cfead99ae0fbe7fb6
tlpackus <tlpackus@gmail.com>
2021-04-10 14:10:10 -0700

    add showError and .submit(event) functions

--- src/main.js
+++ src/main.js
@@ -1,10 +1,26 @@
 import './css/styles.css';
-// import Currency from './currency-converter.js';
+import CurrencyExchangeRate from './currency-converter.js';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
-$(document).ready(function() {
+function showError(error) {
+  $('.showErrors').show();
+  $('.showErrors').text(error);
+  $('.showExchangeValue').hide();
+}
+
+function getElements(response) {
   
+}
+
+$('#exchange-rate').submit(function(event) {
+  event.preventDefault();
+  const currency = $('#currency').val();
+  const usdValue = $('#usd').val();
+  (async function() {
+    const response = await CurrencyExchangeRate.getConversions();
+    getElements(response);
+  });
 });
