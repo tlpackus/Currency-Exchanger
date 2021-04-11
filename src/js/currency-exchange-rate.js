7a519d10bf4f4892a173683c904f989085be6961
tlpackus <tlpackus@gmail.com>
2021-04-10 22:43:28 -0700

    show my current status at getting the getElements() working

--- src/js/currency-exchange-rate.js
+++ src/js/currency-exchange-rate.js
@@ -1,9 +1,8 @@
 export default class CurrencyExchangeRate {
   static async getConversions() {
     try {
-      const response = await fetch (`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD
       `);
-
       if (!response.ok) {
         throw Error(`${response.status} ${response.statusText}`);
       }
--- src/main.js
+++ src/main.js
@@ -1,26 +1,66 @@
-import './css/styles.css';
-import CurrencyExchangeRate from './currency-converter.js';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import CurrencyExchangeRate from './js/currency-exchange-rate.js';
+import ExchangeRate from './exchange-rate.js';
 
-function showError(error) {
-  $('.showErrors').show();
-  $('.showErrors').text(error);
-  $('.showExchangeValue').hide();
-}
+// function showError(text) {
+//   $('.showErrors').show();
+//   $('.showErrors').text(text);
+//   $('.showExchangeValue').hide();
+// }
 
-function getElements(response) {
-  
-}
+// function getElements(response, usd, currency) {
+//   const exchange = new ExchangeRate();
+//   const capitalCurrency = currency.toUpperCase();
+//   if (response.result === "success") {
+//     if (capitalCurrency in response.conversion_rates) {
+//       const returnString = exchange.calculateCurrency(usd, currency, response.conversion_rates[capitalCurrency]);
+//       $('.showExchangeValue').show();
+//       $('.showExchangeValue').text(returnString);
+//       $('.showErrors').hide();
+//     } else {
+//       $('.showErrors').show();
+//       $('.showErrors').text(`${capitalCurrency} is not a valid currency. Please enter a valid currency.`);
+//       $('.showExchangeValue').hide();
+//     }
+//   } else if (response.result === "error") {
+//     $('.showErrors').show();
+//     $('.showErrors').text(`Error: ${response["error-type"]}`);
+//     $('.showExchangeValue').hide();
+//   } else {
+//     $('.showErrors').show();
+//     $('.showErrors').text(response);
+//     $('.showExchangeValue').hide();
+//   }
+// }
+
+// function getElements(response, usd, currency) {
+//   const exchange = new ExchangeRate();
+//   const capitalCurrency = currency.toUpperCase();
+//   if (response.result === "success") {
+//     if (capitalCurrency in response.conversion_rates) {
+//       const returnString = exchange.calcCurrency(usd, currency, response.conversion_rates[capitalCurrency]);
+//       $('.showExchangeValue').show();
+//       $('.showExchangeValue').text(returnString);
+//       $('.showErrors').hide();
+//     } else {
+//       showError(`${capitalCurrency} is not a valid currency. Please select another.`);
+//     }
+//   } else if (response.result === "error") {
+//     showError(`Error: ${response["error-type"]}`);
+//   } else {
+//     showError(`Error: ${response}`);
+//   }
+// }
 
 $('#exchange-rate').submit(function(event) {
   event.preventDefault();
   const currency = $('#currency').val();
-  const usdValue = $('#usd').val();
+  const usd = $('#usd-value').val();
   (async function() {
     const response = await CurrencyExchangeRate.getConversions();
-    getElements(response);
+    getElements(response, usd, currency);
   });
 });
