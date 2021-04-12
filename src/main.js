48950097e368be01dab40f4e44e19129c2c96ef7
tlpackus <tlpackus@gmail.com>
2021-04-12 15:36:39 -0700

    un-comment getElements method to remove run build errors

--- src/main.js
+++ src/main.js
@@ -2,27 +2,26 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import CurrencyExchangeRate from './js/currency-exchange-rate.js';
-import ExchangeRate from './exchange-rate.js';
+import CurrencyExchangeRate from './services/currency-exchange-rate.js';
+import ExchangeRate from './js/exchange-rate.js';
 
-// function showError(text) {
-//   $('.showErrors').show();
-//   $('.showErrors').text(text);
-//   $('.showExchangeValue').hide();
-// }
+function showError(text) {
+  $('.showErrors').show();
+  $('.showErrors').text(text);
+  $('.showExchangeValue').hide();
+}
 
 // function getElements(response, usd, currency) {
 //   const exchange = new ExchangeRate();
-//   const capitalCurrency = currency.toUpperCase();
 //   if (response.result === "success") {
-//     if (capitalCurrency in response.conversion_rates) {
-//       const returnString = exchange.calculateCurrency(usd, currency, response.conversion_rates[capitalCurrency]);
+//     if (currency in response.conversion_rates) {
+//       const returnString = exchange.calcCurrency(usd, currency, response.conversion_rates[currency]);
 //       $('.showExchangeValue').show();
 //       $('.showExchangeValue').text(returnString);
 //       $('.showErrors').hide();
 //     } else {
 //       $('.showErrors').show();
-//       $('.showErrors').text(`${capitalCurrency} is not a valid currency. Please enter a valid currency.`);
+//       $('.showErrors').text(`${currency} is not a valid currency. Please enter a another currency.`);
 //       $('.showExchangeValue').hide();
 //     }
 //   } else if (response.result === "error") {
@@ -36,31 +35,44 @@ import ExchangeRate from './exchange-rate.js';
 //   }
 // }
 
-// function getElements(response, usd, currency) {
-//   const exchange = new ExchangeRate();
-//   const capitalCurrency = currency.toUpperCase();
-//   if (response.result === "success") {
-//     if (capitalCurrency in response.conversion_rates) {
-//       const returnString = exchange.calcCurrency(usd, currency, response.conversion_rates[capitalCurrency]);
-//       $('.showExchangeValue').show();
-//       $('.showExchangeValue').text(returnString);
-//       $('.showErrors').hide();
-//     } else {
-//       showError(`${capitalCurrency} is not a valid currency. Please select another.`);
-//     }
-//   } else if (response.result === "error") {
-//     showError(`Error: ${response["error-type"]}`);
-//   } else {
-//     showError(`Error: ${response}`);
-//   }
-// }
+function getElements(response, usd, currency) {
+  const exchange = new ExchangeRate();
+  if (response.result === "success") {
+    if (currency in response.conversion_rates) {
+      const returnString = exchange.calcCurrency(usd, currency, response.conversion_rates[currency]);
+      $('.showExchangeValue').show();
+      $('.showExchangeValue').text(returnString);
+      $('.showErrors').hide();
+    } else {
+      showError(`${currency} is not a valid currency. Please select another.`);
+    }
+  } else if (response.result === "error") {
+    showError(`Error: ${response["error-type"]}`);
+  } else {
+    showError(`Error: ${response}`);
+  }
+}
 
-$('#exchange-rate').submit(function(event) {
-  event.preventDefault();
-  const currency = $('#currency').val();
-  const usd = $('#usd-value').val();
-  (async function() {
-    const response = await CurrencyExchangeRate.getConversions();
-    getElements(response, usd, currency);
+$(document).ready(function() {
+  $('#exchange-rate').submit(function(event) {
+    event.preventDefault();
+    const usd = parseInt($('#usd-value').val());
+    const currency = $('#currency').val().toUpperCase();
+    (async function() {
+      const response = await CurrencyExchangeRate.getConversions();
+      getElements(response, usd, currency);
+    });
   });
 });
+
+
+
+// $('#exchange-rate').submit(function(event) {
+//   event.preventDefault();
+//   const currency = $('#currency').val();
+//   const usd = $('#usd-value').val();
+//   (async function() {
+//     const response = await CurrencyExchangeRate.getConversions();
+//     getElements(response, usd, currency);
+//   });
+// });
